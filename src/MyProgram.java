b4386546f6ce237124c84d752c9bad1302342490
bubliksite <serj.solntsev@gmail.com>
2021-01-15 01:54:20 +0300

    Lesson 1

---
+++ src/MyProgram.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MyProgram {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter a: ");
+    int a = scanner.nextInt();
+
+    System.out.println("Enter b: ");
+    int b = scanner.nextInt();
+
+    System.out.println("Sum: ");
+    System.out.println(a + b);
+  }
+}
